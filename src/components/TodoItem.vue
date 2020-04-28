e79a26f7846695330589f1c9d9854b45ffa7f977
Chris Carr <christocarr@outlook.com>
2020-04-28 14:47:09 +0100

    Add new component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ todo.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"]
+}
+</script>
+
+<style scoped>
+
+</style>
