fd80d8a8ad7873452ef6a4819141d0a35e2f5b5c
Chris Carr <christocarr@outlook.com>
2020-04-30 09:52:49 +0100

    Add limit to json placeholder request

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ export default {
   },
   created() {
     axios
-      .get('https://jsonplaceholder.typicode.com/todos')
+      .get('https://jsonplaceholder.typicode.com/todos?_limit=5')
       .then(res => this.todos = res.data)
       .catch(err => console.log(err))
   },
